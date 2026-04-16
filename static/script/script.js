4b54278f492b3a7d116cca44c9b09909645b6db9
AliceIndex <hh48691537@gmail.com>
2026-04-16 23:30:23 +0900

    バージョン部分の修正

--- static/script/script.js
+++ static/script/script.js
@@ -4,7 +4,7 @@
  * Unauthorized copying of this file, via any medium is strictly prohibited.
  */
 
-const CURRENT_VERSION = 1
+const CURRENT_VERSION = '1';
 
 document.addEventListener('DOMContentLoaded', () => {
 
